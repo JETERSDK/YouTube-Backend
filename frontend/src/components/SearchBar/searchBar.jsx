fd43a327a04ee2888d10bf94af757c2bece9db80
Ennin Jeter <jeterennin@gmail.com>
2022-05-03 14:55:34 -0500

    Updated SearchBar + VideoPlayer

---
+++ frontend/src/components/SearchBar/searchBar.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                searchQuery: '',
+            }
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.searchVideo(this.state.searchQuery);
+    }
+
+    render() {
+        return (
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+                <input type="text" name="searchQuery" placeholder="Search..."
+                onChange={this.handleChange} value={this.searchQuery}/>
+                <button className="btn btn-primary" type="submit">Submit </button>
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
